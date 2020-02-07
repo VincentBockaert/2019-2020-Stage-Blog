d743ce035c2893fd334db7b1394eae489fcc6987
Vincent Bockaert <bockaert.vincent1997@gmail.com>
2020-02-07 18:29:21 +0100

    basic complete containerization done

--- myBlog/myBlog/settings.py
+++ myBlog/myBlog/settings.py
@@ -74,10 +74,34 @@ WSGI_APPLICATION = 'myBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {}
+
+# if os.getenv("MYSQL_PORT") is not None:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': os.getenv("MYSQL_DB_NAME"),
+#             'USER': os.getenv("MYSQL_USER"),
+#             'PASSWORD': os.getenv("MYSQL_PASSWORD"),
+#             'HOST': os.getenv("MYSQL_HOST"),
+#             'PORT': os.getenv("MYSQL_PORT"),
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("MYSQL_DB_NAME"),
+        'USER': os.getenv("MYSQL_USER"),
+        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
+        'HOST': os.getenv("MYSQL_HOST"),
+        'PORT': os.getenv("MYSQL_PORT"),
     }
 }
 
