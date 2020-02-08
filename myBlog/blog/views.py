a491f794ed204f65ff717832f1ca0c7901401fb6
Vincent Bockaert <bockaert.vincent1997@gmail.com>
2020-02-08 17:23:04 +0100

    Added title to blog web pages

--- myBlog/blog/views.py
+++ myBlog/blog/views.py
@@ -9,11 +9,11 @@ from .models import Blog
 def index(request):
     # return render(request,'blog.html')
     latest_blog_posts_list = Blog.objects.order_by('-pub_date')
-    context = {'latest_blog_posts_list': latest_blog_posts_list}
+    context = {'latest_blog_posts_list': latest_blog_posts_list, 'title': 'Blog Index'}
     return render(request, 'blog/index.html', context)
 
 def detail(request, blog_id):
     blog = Blog.objects.get(pk=blog_id)
     content = markdown.markdown(blog.content)
-    context = {'content': content}
+    context = {'content': content, 'title': 'Blog Detailed'}
     return render(request,'blog/details.html',context=context)
